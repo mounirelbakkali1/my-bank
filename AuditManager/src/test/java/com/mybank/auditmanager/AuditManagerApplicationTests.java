4dd9b7ecf540afe6ff8f6cff0bd2336a043291b4
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-05-14 21:26:30 +0100

    added separate module for managing logs and application auditing

---
+++ AuditManager/src/test/java/com/mybank/auditmanager/AuditManagerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mybank.auditmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class AuditManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
